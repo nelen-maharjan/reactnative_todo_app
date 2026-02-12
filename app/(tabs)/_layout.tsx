846c2e89d4fd9cd30504af2360c33b50ee59819f
Nelen Maharjan <neleen11@gmail.com>
2026-02-12 19:50:59 +0545

    toggle darkMode

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import useTheme from "@/hooks/useTheme";
 
 const TabsLayout = () => {
+    const {colors} = useTheme()
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "green",
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
         tabBarStyle: {
-          backgroundColor: "#1e293b",
+          backgroundColor: colors.surface,
           borderTopWidth: 1,
-          borderTopColor: "yellow",
+          borderTopColor: colors.border,
           height: 90,
           paddingBottom: 30,
           paddingTop: 10,
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
+import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
+    <ThemeProvider>
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
     </Stack>
+    </ThemeProvider>
   );
 }
