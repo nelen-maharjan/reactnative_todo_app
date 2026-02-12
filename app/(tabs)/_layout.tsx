b47d6d2473b5ed67f85538da4d14cbbbef2451a8
Nelen Maharjan <neleen11@gmail.com>
2026-02-12 19:28:21 +0545

    tab nagivation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "green",
+        tabBarStyle: {
+          backgroundColor: "#1e293b",
+          borderTopWidth: 1,
+          borderTopColor: "yellow",
+          height: 90,
+          paddingBottom: 30,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: "600",
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Todos",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgreen"
+  },
+  content: {
+   fontSize: 18,
+   color: "white"
+  },
+})
