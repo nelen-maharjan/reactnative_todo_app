170a3b3d9e815488f216b145d890a77396a2835f
Nelen Maharjan <neleen11@gmail.com>
2026-02-14 19:21:27 +0545

    todo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,32 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme()
+  const {toggleDarkMode} = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+
+  console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo);
+
+  const clearAllTodos = useMutation(api.todos.clearAllTodos);
+
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text style={styles.content}>Toggle Mode</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => addTodo({ text: "New Todo" })}>
+        <Text style={styles.content}>Add Todo</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => clearAllTodos()}>
+        <Text style={styles.content}>Clear All Todos</Text>
       </TouchableOpacity>
     </View>
   );
