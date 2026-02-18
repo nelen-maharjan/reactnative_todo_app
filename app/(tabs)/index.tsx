cb970e442b5b6768a9f38d87dec5120931bd51ad
Nelen Maharjan <neleen11@gmail.com>
2026-02-18 18:32:57 +0545

    home styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { api } from "@/convex/_generated/api";
-import useTheme from "@/hooks/useTheme";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme();
+  const {toggleDarkMode, colors} = useTheme();
+
+  const styles = createStyles(colors);
 
   const todos = useQuery(api.todos.getTodos);
 
@@ -32,13 +34,18 @@ export default function Index() {
   );
 }
 
-const styles = StyleSheet.create({
+const createStyles = (colors: ColorScheme) =>{
+  const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    gap: 10,
+    backgroundColor: colors.bg,
   },
   content: {
    fontSize: 18,
   },
-})
+});
+  return styles;
+}
